030d5d32685881571807e58510e69cf00cb3ee09
jaren <jaren.mendelsohn@gmail.com>
2017-05-19 12:37:56 -0700

    Better collisions

--- app/src/main/java/io/github/automaticspork/aegis/components/ShieldSprite.java
+++ app/src/main/java/io/github/automaticspork/aegis/components/ShieldSprite.java
@@ -19,19 +19,26 @@ import io.github.automaticspork.aegis.Vector;
 public class ShieldSprite extends CollidableSprite {
     public float minAngle = 270;
     public float sweep = 120;
+    public float diffRadius = 10;
+
+    private CollidableSprite innerCollision;
 
     public ShieldSprite() {
-        super(new Vector(), new Paint(), 100);
+        super(new Vector(), new Paint(), 150);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(10);
         paint.setColor(Color.parseColor("#00acc1"));
+
+        innerCollision = new CollidableSprite(position, paint, radius - diffRadius);
     }
 
     @Override
     public void update(List<Sprite> sprites, GameView view) {
         super.update(sprites, view);
 
+        paint.setStrokeWidth(diffRadius);
         position = new Vector(view.screenSize.x / 2, view.screenSize.y / 2);
+        innerCollision.radius = radius - diffRadius;
+        innerCollision.position = position;
 
         Vector diff = view.lastTouch.clone();
         diff.subtract(position);
@@ -59,7 +66,7 @@ public class ShieldSprite extends CollidableSprite {
         diff.subtract(position);
         double angle = Math.toDegrees(Math.atan2(diff.y, diff.x));
         if (angle < 0) angle = 360 + angle;
-        if (!super.collides(other)) return false;
+        if (!super.collides(other) || innerCollision.collides(other)) return false;
         if (minAngle + sweep > 360) return angle > minAngle || angle < (minAngle + sweep) % 360;
         return angle > minAngle && angle < minAngle + sweep;
     }
